30281f629dc4df58429cc79f7f30f28c3d7241ab
arjenvklaveren <arjenvklaveren@gmail.com>
2019-05-14 16:24:24 +0200

    object to mouse ps

--- Source Code/game.js
+++ Source Code/game.js
@@ -7,6 +7,7 @@ var myGameArea = {
     start : function(){
         this.canvas.width = 200;
         this.canvas.height = 400;
+        this.canvas.onmousemove = setPlayerPosition(event);
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         
@@ -14,25 +15,20 @@ var myGameArea = {
 }
 
 var player = {
-    x:myGameArea.canvas.width/2,
-    y:myGameArea.canvas.height-100,
+    x:myGameArea.canvas.width/3,
+    y:myGameArea.canvas.height+75,
     speed: 20
 };
 
-function move(e) {
-    if(e.keyCode == 37){
-        player.x -= player.speed;
-    }
-    if(e.keyCode == 39){
-        player.x += player.speed;
-    }
+function setPlayerPosition(e) {
+    player.x = getMousePos(this.canvas, e).x;
 }
-
-document.onkeydown = move;
 
 function clearCanvas() {
     myGameArea.context.clearRect(0,0, myGameArea.canvas.width, myGameArea.canvas.height);
 }
+
+window.addEventListener("mousemove", setPlayerPosition, false);
 
 function drawPlayer(){
     var x = player.x;
@@ -42,6 +38,14 @@ function drawPlayer(){
     myGameArea.context.lineTo(x+15, y+50);
     myGameArea.context.lineTo(x-15, y+50);
     myGameArea.context.fill();
+}
+
+function getMousePos(canvas, evt) {
+    var rect = myGameArea.canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
 }
 
 function update(){
